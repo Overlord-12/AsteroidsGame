ad53a0faf3e102576881fbfccbcdf76605173cfd
@d.argastcev <daniil.argastsev@solbeg.com>
2022-01-13 13:08:37 +0300

    Add project files.

---
+++ AsteroidsGame/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsteroidsGame
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var form = new Form()
+            {
+                MinimumSize = new System.Drawing.Size(800, 600),
+                MaximumSize = new System.Drawing.Size(800, 600),
+                MaximizeBox = false,
+                MinimizeBox = false,
+                StartPosition = FormStartPosition.CenterScreen,
+                Text = "Asteroids"
+            };
+            form.Show();
+
+            SceneManager    // Обратимся к менеджеру сцен
+                .Get()
+                .Init<MenuScene>(form) // Попросим загрузить меню игры
+                .Draw();
+
+            Application.Run(form);
+        }
+    }
+}
